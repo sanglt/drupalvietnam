de517ae2dfbe9cf02317e434b0abef740c7dbd24
Lê Thanh Sang <me@sanglt.com>
2010-07-01 17:28:36 +0700

    Layout lại nút search

--- themes/drupalvietnam/template.php
+++ themes/drupalvietnam/template.php
@@ -4,3 +4,22 @@ function drupalvietnam_preprocess_html(&$variables) {
   // Thêm blueprint css dành cho IE
   drupal_add_css(path_to_theme() . '/css/blueprint/ie.css', array('weight' => CSS_THEME, 'browsers' => array('IE' => 'lt IE 8', '!IE' => FALSE), 'preprocess' => FALSE));
 }
+
+
+function drupalvietnam_preprocess_page(&$variables) {
+  if (isset($variables['main_menu'])) {
+    $variables['primary_nav'] = theme('links__system_main_menu', array(
+      'links' => $variables['main_menu'],
+      'attributes' => array(
+        'class' => array('links', 'main-menu'),
+      ),
+      'heading' => array(
+        'text' => t('Main menu'),
+        'level' => 'h2',
+        'class' => array('element-invisible'),
+      )
+    ));
+  } else {
+    $variables['primary_nav'] = FALSE;
+  }
+}
--- themes/drupalvietnam/tpl/page.tpl.php
+++ themes/drupalvietnam/tpl/page.tpl.php
@@ -72,52 +72,48 @@
 
     <div id="header"><div class="section container clearfix">
     
-      <?php if ($main_menu): ?>
-        <div id="navigation"><div class="section">
-          <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'clearfix')), 'heading' => t('Main menu'))); ?>
-        </div></div> <!-- /.section, /#navigation -->
+      <?php if ($primary_nav): ?>
+        <div id="navigation" class="clearfix">
+          <?php print $primary_nav; ?>
+        </div> <!-- /#navigation -->
       <?php endif; ?>
-
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      <?php endif; ?>
-
-      <?php if ($site_name || $site_slogan): ?>
-        <div id="name-and-slogan">
-          <?php if ($site_name): ?>
-            <?php if ($title): ?>
-              <div id="site-name"><strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </strong></div>
-            <?php else: /* Use h1 when the content title is empty */ ?>
-              <h1 id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </h1>
+      
+      <div id="header-content">
+        <div id="header-left" class="span-12">
+        <?php if ($site_name || $site_slogan): ?>
+          <div id="name-and-slogan">
+            <?php if ($site_name): ?>
+                <h1 id="site-name">
+                  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
+                </h1>
             <?php endif; ?>
-          <?php endif; ?>
-
-          <?php if ($site_slogan): ?>
-            <div id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-        </div> <!-- /#name-and-slogan -->
-      <?php endif; ?>
+  
+            <?php if ($site_slogan && drupal_is_front_page()): ?>
+              <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
+              <?php print render($page['header_front']); ?>
+            <?php endif; ?>
+          </div> <!-- /#name-and-slogan -->
+        <?php endif; ?>
+        </div>
+        <div id="header-right" class="prepend-2 span-10 last">
+        <?php print render($page['search']); ?>
+        </div>
+      </div>
+      
 
       <?php print render($page['header']); ?>
 
     </div></div> <!-- /.section, /#header -->
 
+
+    <div id="main-wrapper"><div id="main" class="clearfix container showgrid">
     <?php if ($breadcrumb): ?>
       <div id="breadcrumb"><?php print $breadcrumb; ?></div>
     <?php endif; ?>
 
     <?php print $messages; ?>
-
-    <div id="main-wrapper"><div id="main" class="clearfix container showgrid">
-
+    
       <div id="content" class="column"><div class="section">
-        <?php if ($page['highlight']): ?><div id="highlight"><?php print render($page['highlight']); ?></div><?php endif; ?>
         <a id="main-content"></a>
         <?php print render($title_prefix); ?>
         <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
@@ -128,18 +124,6 @@
         <?php print render($page['content']); ?>
         <?php print $feed_icons; ?>
       </div></div> <!-- /.section, /#content -->
-
-      <?php if ($page['sidebar_first']): ?>
-        <div id="sidebar-first" class="column sidebar"><div class="section">
-          <?php print render($page['sidebar_first']); ?>
-        </div></div> <!-- /.section, /#sidebar-first -->
-      <?php endif; ?>
-
-      <?php if ($page['sidebar_second']): ?>
-        <div id="sidebar-second" class="column sidebar"><div class="section">
-          <?php print render($page['sidebar_second']); ?>
-        </div></div> <!-- /.section, /#sidebar-second -->
-      <?php endif; ?>
 
     </div></div> <!-- /#main, /#main-wrapper -->
 
