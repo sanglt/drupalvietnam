83e0b715b858ab8b36f40b5d2527be270a8a422f
Andy Truong <andy@toila.net>
2011-03-02 20:07:12 -0800

    + inner join example + fetch results example

--- drupal.database-api.php
+++ drupal.database-api.php
@@ -30,6 +30,21 @@
  *    ->condition('name', 'Jane')
  *    ->execute();
  * @endcode
+ *
+ * Thí dụ nối bảng INNER JOIN:
+ * @code
+ *   db_select('node', 'n')
+ *     ->innerJoin('users', 'u', 'n.uid = u.uid');
+ * @endcode
+ *
+ * Lấy kết quả truy vấn:
+ * @code
+ *   // Get uid, name of users who access Drupal last 10 minutes
+ *   $query = db_select('users', 'u')->fields('u', array('uid', 'name'));
+ *   $query->condition('u.access', time() - 10 * 60, '>=');
+ *   $users = $query->execute()->fetAll();
+ *   print_r($users);
+ * @endcode
  */
 /**
  * @} End of "defgroup drupal_database_usage"
