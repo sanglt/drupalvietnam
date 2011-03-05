7df7e6c9f845ffdc3231b0d5961142dceb7597bd
Andy T <andy@toila.net>
2011-03-06 06:45:23 +0700

    * Use HEAD branch of examples (we will create an other branch for drupal 6 later)

--- examples/README.txt
+++ examples/README.txt
@@ -1,8 +1,11 @@
-# $Id: README.txt,v 1.1.2.2 2010/09/26 13:16:24 rfay Exp $
 
 Examples modules
 
-Đây là tập hợp các modules thí dụ, trình bày những tính năng của Drupal.
-Chỉ dành cho người dùng là developer.
+This set of modules is intended to show how to use various Drupal features.
+It's intended only for developers.
 
-Mọi trở ngại gì, xin tạo issue ở @link http://drupal.org/project/examples trang nhà của Example Modules @endlink.
+If you find a problem, bad comment, poor usage, out-of-date API usage,
+etc., please post an issue in the issue queue at
+http://drupal.org/project/examples.
+
+
--- examples/vertical_tabs_example/vertical_tabs_example.js
+++ examples/vertical_tabs_example/vertical_tabs_example.js
@@ -1,18 +1,24 @@
-// $Id: vertical_tabs_example.js,v 1.1.2.2 2010/02/10 21:46:26 davereid Exp $
 
-// The following line will prevent a JavaScript error if this file is included
-// and vertical tabs are disabled.
-Drupal.verticalTabs = Drupal.verticalTabs || {};
+(function ($) {
 
-Drupal.verticalTabs.vertical_tabs_example = function() {
-  if ($('#edit-vertical-tabs-example-enabled').attr('checked')) {
-    // We show here the container with the custom settings form.
-    $('#edit-vertical-tabs-example-custom-setting-wrapper').parent().show('fast')
-    return Drupal.checkPlain($('#edit-vertical-tabs-example-custom-setting').val());
+/**
+ * Custom summary for the module vertical tab.
+ */
+Drupal.behaviors.vertical_tabs_exampleFieldsetSummaries = {
+  attach: function (context) {
+    // Use the fieldset class to identify the vertical tab element
+    $('fieldset#edit-vertical-tabs-example', context).drupalSetSummary(function (context) {
+      // Depending on the checkbox status, the settings will be customized, so
+      // update the summary with the custom setting textfield string or a use a
+      // default string.
+      if ($('#edit-vertical-tabs-example-enabled', context).attr('checked')) {
+        return Drupal.checkPlain($('#edit-vertical-tabs-example-custom-setting', context).val());
+      }
+      else {
+        return Drupal.t('Using default');
+      }
+    });
   }
-  else {
-    // We hide here the container with the custom settings form.
-    $('#edit-vertical-tabs-example-custom-setting-wrapper').parent().hide('fast')
-    return Drupal.t('Using default');
-  }
-}
+};
+
+})(jQuery);
