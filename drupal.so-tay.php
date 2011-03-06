ab595d5629289c2e132ef9961127c81b9c280f8c
Andy Truong <andy@toila.net>
2011-03-05 16:39:24 -0800

    Trying to make it works!

--- drupal.so-tay.php
+++ drupal.so-tay.php
@@ -1,11 +1,13 @@
 <?php
-// $Id$
 
 /**
+ * @file
+ * Sổ tay Drupal
+ *
  * - Giới thiệu tổng quát
  *   - @link http://groups.drupal.org/node/1958 Drupal là gì? @endlink
  *   - @link http://groups.drupal.org/node/4719 Các website Việt Nam sử dụng Drupal @endlink
  * - Cách cài đặt và cấu hình Drupal
- *  - @link http://groups.drupal.org/node/5619 Yêu cầu hệ thống khi chạy Drupal @endlink
+ *   - @link http://groups.drupal.org/node/5619 Yêu cầu hệ thống khi chạy Drupal @endlink
+ *   - @link http://groups.drupal.org/node/5569 Cài đặt và gỡ bỏ Module @endlink
  */
-
