c5d28527600592c77358013455bbdc5b8b6e75eb
Andy T <andy@toila.net>
2011-03-06 07:34:38 +0700

    * Updating links.

---
+++ drupal.so-tay.php
@@ -0,0 +1,11 @@
+<?php
+// $Id$
+
+/**
+ * - Giới thiệu tổng quát
+ *   - @link http://groups.drupal.org/node/1958 Drupal là gì? @endlink
+ *   - @link http://groups.drupal.org/node/4719 Các website Việt Nam sử dụng Drupal @endlink
+ * - Cách cài đặt và cấu hình Drupal
+ *  - @link http://groups.drupal.org/node/5619 Yêu cầu hệ thống khi chạy Drupal @endlink
+ */
+
